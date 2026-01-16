6c28d05ec8c98463356ffbe98263cf66439644db
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-01-16 15:31:35 +0700

    ref: change the works styling in works and experience

--- src/routes/works-and-experience/(list)/+page.svelte
+++ src/routes/works-and-experience/(list)/+page.svelte
@@ -8,10 +8,10 @@
 	<div class="space-y-3">
 		{#each data.works as work (work.slug)}
 			<div class="flex gap-4">
-				<span class="w-24 text-[#4c4f69] dark:text-[#cdd6f4]">{work.displayDate}</span>
+				<span class="w-30 text-[#4c4f69] dark:text-[#cdd6f4]">{work.displayDate}</span>
 				<a
 					href="/works-and-experience/{work.slug}"
-					class="text-[#1e66f5] hover:underline dark:text-[#89b4fa]"
+					class="text-[#1e66f5] no-underline hover:underline dark:text-[#89b4fa]"
 				>
 					{work.title}
 				</a>
