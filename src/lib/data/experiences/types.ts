08a8d452ef748936b601b99f5ce6eb59fc5a1704
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-07-03 18:08:52 +0700

    feat: add experience types interface

---
+++ src/lib/data/experiences/types.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+	title: string;
+	organization?: string;
+	period: string;
+	sortDate: string;
+	description: string;
+	images?: { src: string; alt: string }[];
+	links?: { text: string; url: string }[];
+}
