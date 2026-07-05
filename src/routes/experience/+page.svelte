ff3e4a5c94e2be05ff47693c88080e1ebc028abc
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-07-05 17:59:59 +0700

    refactor: split experience timeline into metadata/content columns on desktop

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -10,17 +10,13 @@
 	></div>
 
 	{#each data.experiences as exp, i}
-		{@const isLeft = i % 2 === 0}
-
-		<div class="relative pb-8 last:pb-0">
+		<div class="relative pb-8 md:-mt-8 first:md:mt-0 last:pb-0">
 			<div
 				class="absolute left-[6px] top-1 h-3 w-3 rounded-full border-2 border-link bg-bg md:left-1/2 md:-translate-x-1/2"
 			></div>
 
-			<div class="pl-8 md:flex md:pl-0">
-				<div
-					class="md:w-1/2 {isLeft ? 'md:mr-auto md:pr-6 md:text-right' : 'md:ml-auto md:pl-6'}"
-				>
+			<div class="pl-8 md:grid md:grid-cols-[1fr_auto_1fr] md:gap-6 md:pl-0">
+				<div class="md:text-right">
 					<span class="text-sm text-muted">{exp.period}</span>
 					<h3 class="text-lg font-semibold text-heading">
 						{exp.title}
@@ -28,12 +24,17 @@
 					{#if exp.organization}
 						<p class="text-sm text-body">{exp.organization}</p>
 					{/if}
-					<p class="mt-2 text-sm leading-relaxed text-body">
+				</div>
+
+				<div class="hidden md:block md:w-8"></div>
+
+				<div>
+					<p class="text-sm leading-relaxed text-body">
 						{exp.description}
 					</p>
 
 					{#if exp.images}
-						<div class="mt-3 flex flex-wrap gap-2 {isLeft ? 'md:justify-end' : ''}">
+						<div class="mt-3 flex flex-wrap gap-2">
 							{#each exp.images as img}
 								<img
 									src={img.src}
@@ -46,9 +47,7 @@
 					{/if}
 
 					{#if exp.links}
-						<div
-							class="mt-2 flex flex-wrap gap-x-4 gap-y-1 {isLeft ? 'md:justify-end' : ''}"
-						>
+						<div class="mt-2 flex flex-wrap gap-x-4 gap-y-1">
 							{#each exp.links as link}
 								<a
 									href={link.url}
