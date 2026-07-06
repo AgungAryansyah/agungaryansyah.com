3c192ee3ffb9cdbe43a4d11fcf22d6fd0a993f4f
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-07-06 13:56:46 +0700

    chore: prune placeholder writings, tidy writing list markup

--- src/routes/writings/(list)/+page.svelte
+++ src/routes/writings/(list)/+page.svelte
@@ -4,7 +4,7 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
-<div class="space-y-6 stagger-children">
+<div class="stagger-children space-y-6">
 	<script lang="ts">
 		import type { PageData } from './$types';
 
@@ -17,12 +17,10 @@
 			<div class="space-y-3">
 				{#each group.works as work (work.slug)}
 					<div class="flex">
-						<span class="w-20 flex-none whitespace-nowrap text-body"
-							>{work.displayDate}</span
-						>
+						<span class="w-20 flex-none whitespace-nowrap text-body">{work.displayDate}</span>
 						<a
 							href="/writings/{work.slug}"
-							class="text-link font-semibold underline decoration-accent decoration-1 underline-offset-2 hover:decoration-link"
+							class="font-semibold text-link underline decoration-accent decoration-1 underline-offset-2 hover:decoration-link"
 						>
 							{work.title}
 						</a>
@@ -31,14 +29,4 @@
 			</div>
 		</section>
 	{/each}
-
-	<p>
-		(*i'm currently still wriring about my other works, for now you can check my
-		<a
-			href="https://github.com/AgungAryansyah"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="text-link font-semibold underline decoration-accent decoration-1 underline-offset-2 hover:decoration-link">github</a
-		>)
-	</p>
 </div>
